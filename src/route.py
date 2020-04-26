0652748e8bd7f7996fe8746e2e30d255652dbe45
rknol <rknol@deloitte.nl>
2020-04-26 10:52:56 +0200

    put json directly to db

--- src/route.py
+++ src/route.py
@@ -23,25 +23,35 @@ def route():
 def create_company():
     if not request.json or 'name' not in request.json:
         abort(400)
-    company = {
-        'type': request.json['type'],
-        'name': request.json['name'],
-        'address': request.json['address'],
-        'businessType': request.json['businessType'],
-        'issues': request.json['issues'],
-        'numberOfJobs': request.json['numberOfJobs'],
-        'whatHappened': request.json['whatHappened'],
-        'whatNeed': request.json['whatNeed'],
-        'socialNetworks': request.json['socialNetworks'],
-        'email': request.json['email'],
-        'phone': request.json['phone'],
-        'coords': {
-            'lat': float(request.json['coords']['lat']),
-            'lng': float(request.json['coords']['lng'])
-        }
-    }
-    app.mongo.companyDB.create(company)
-    return JSONEncoder().encode(company), 201
+    # company = Company(**request.json)
+    # print(company)
+    # company = {
+    #     'type': request.json['type'],
+    #     'name': request.json['name'],
+    #     'address': request.json['address'],
+    #     'business': request.json['business'],
+    #     'business_other': request.json['business_other'],
+    #     'issues': request.json['issues'],
+    #     'issues_other': request.json['issues_other'],
+    #     'help': request.json['help'],
+    #     'help_other': request.json['help_other'],
+    #     'business_help': request.json['business_help'],
+    #     'business_help_other': request.json['business_help_other'],
+    #     'jobs': request.json['jobs'],
+    #     'problem': request.json['problem'],
+    #     'needs': request.json['needs'],
+    #     'social': request.json['social'],
+    #     'email': request.json['email'],
+    #     'phone': request.json['phone'],
+    #     'company_name': request.json['company_name'],
+    #     'story': request.json['story'],
+    #     'coords': {
+    #         'lat': float(request.json['coords']['lat']),
+    #         'lng': float(request.json['coords']['lng'])
+    #     }
+    # }
+    app.mongo.companyDB.create(request.json)
+    return JSONEncoder().encode(request.json), 201
 
 
 @app.flask.route("/api/v1/company/<company_id>", methods=["GET"])
@@ -59,26 +69,48 @@ def get_all_markers():
 
 class Company:
     def __init__(self,
-                 name,
-                 address,
-                 business_type,
-                 issues,
-                 number_of_jobs,
-                 what_happened,
-                 what_need,
-                 social_networks,
-                 email,
-                 phone):
+                 type=None,
+                 name=None,
+                 address=None,
+                 business=None,
+                 business_type=None,
+                 issues=None,
+                 issues_other=None,
+                 help=None,
+                 help_other=None,
+                 business_help=None,
+                 business_help_other=None,
+                 jobs=None,
+                 problem=None,
+                 needs=None,
+                 social=None,
+                 email=None,
+                 phone=None,
+                 company_name=None,
+                 story=None,
+                 coords=None
+                    ):
+        self.type = type
         self.name = name
         self.address = address
-        self.businessType = business_type
+        self.business = business
+        self.issues_other = issues_other
+        self.help = help
+        self.help_other = help_other
+        self.business_type = business_type
         self.issues = issues
-        self.numberOfJobs = number_of_jobs
-        self.whatHappened = what_happened
-        self.whatNeed = what_need
-        self.socialNetworks = social_networks
+        self.business_help = business_help
+        self.business_help_other = business_help_other
+        self.jobs = jobs
+        self.problem = problem
+        self.needs = needs
+        self.social = social
         self.email = email
         self.phone = phone
+        self.company_name = company_name
+        self.story = story
+        self.coords = coords
+
 
 
 class JSONEncoder(json.JSONEncoder):
