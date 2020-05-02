d527993b56194285283e69a7c4b0cf6f935214b2
rknol <rknol@deloitte.nl>
2020-05-02 13:03:07 +0200

    implemented APIs from fork, cleaner API validation WIP

--- src/database/companyDB.py
+++ src/database/companyDB.py
@@ -15,57 +15,26 @@ class CompanyDB(object):
         return found_company
 
     def get_all_markers(self):
-        # database = self.collection.find()
-        # markerList = []
-        # for x in database:
-        #     markerList.append((x["coords"], x["_id"]))
-        # print(markerList)
-        return [[x["coords"], x["_id"], x["type"]] for x in self.collection.find()]
+        return [[x["coords"], x["_id"], x["type"]] for x in self.collection.find() if "coords" in x and x["coords"]]
 
-    # Read Record
-    # def read(self, name, arg=False):
-    #
-    #     if arg:
-    #
-    #         return self.api[name].find_one(arg['query'])
-    #
-    #     else:
-    #
-    #         items = []
-    #
-    #         for item in self.api[name].find():
-    #             item.pop('_id')
-    #
-    #             items.append(item)
-    #
-    #         return items
+    def find_company(self, query):
+        myquery = {"$or": [{"$text": {"$search": query}}, {"jobs": query}]}
+        result = self.collection.find(myquery)
+        return [{key: val for key, val in item.items() if key not in ["email", "phone", "coords"]} for item in result]
 
-    # Create Record
+
+    def get_counters(self):
+        all_companies = self.collection.find().count()
+        jobs_danger = sum([int(item["jobs"])for item in self.collection.find({"type": "needHelp"})])
+        companies_rescued = self.collection.find({"type": "story"})
+
+        return {
+            "all_companies": all_companies,
+            "jobs_danger": jobs_danger,
+            "companies_rescued": companies_rescued.count(),
+            "jobs_rescued": 0
+        }
+
     def create(self, company):
         self.collection.insert_one(company)
         return self.collection.find_one({'name': company.get('name')})
-        #
-        # if arg:
-        #
-        #     co = self.api[name]
-        #
-        #     id = arg['id'] = co.count() + 1
-        #
-        #     if co.insert_one(arg):
-        #         return co.find_one({'id': id})
-
-    # Update
-    # def update(self, name, arg):
-    #
-    #     co = self.api[name]
-    #
-    #     if co.update_one(arg['query'], {'$set': arg['data']}).matched_count > 0:
-    #         return co.find_one(arg['query'])
-    #
-    # # Delete
-    # def delete(self, name, arg=False):
-    #
-    #     co = self.api[name]
-    #
-    #     if not co.delete_one(arg['query']):
-    #         return
--- src/route.py
+++ src/route.py
@@ -23,6 +23,85 @@ def route():
 def create_company():
     if not request.json or 'name' not in request.json:
         abort(400)
+
+    app.mongo.companyDB.create(request.json)
+    return JSONEncoder().encode(request.json), 201
+
+
+@app.flask.route("/api/v1/company/<company_id>", methods=["GET"])
+def get_company_by_id(company_id):
+    company = app.mongo.companyDB.get_company_by_id(company_id)
+    app.mongo.companyDB.get_all_markers()
+    return JSONEncoder().encode(company), 200
+
+
+@app.flask.route("/api/v1/getAllMarkers")
+def get_all_markers():
+    markers = app.mongo.companyDB.get_all_markers()
+    return JSONEncoder().encode(markers), 200
+
+
+@app.flask.route("/api/v1/findCompany/<query>")
+def find_company(query):
+    companies = app.mongo.companyDB.find_company(query)
+    return JSONEncoder.encode(companies), 200
+
+
+@app.flask.route("/api/v1/counters")
+def get_counters():
+    companies = app.mongo.companyDB.get_counters()
+    return JSONEncoder.encode(companies), 200
+
+# class Company:
+#     def __init__(self):
+#         emailadress : str
+#         type : str
+
+# class Company:
+#     def __init__(self,
+#                  type=None,
+#                  name=None,
+#                  address=None,
+#                  business=None,
+#                  business_type=None,
+#                  issues=None,
+#                  issues_other=None,
+#                  help=None,
+#                  help_other=None,
+#                  business_help=None,
+#                  business_help_other=None,
+#                  jobs=None,
+#                  problem=None,
+#                  needs=None,
+#                  social=None,
+#                  email=None,
+#                  phone=None,
+#                  company_name=None,
+#                  story=None,
+#                  coords=None
+#                     ):
+#         emailadress : str
+#         self.type = type
+#         self.name = name
+#         self.address = address
+#         self.business = business
+#         self.issues_other = issues_other
+#         self.help = help
+#         self.help_other = help_other
+#         self.business_type = business_type
+#         self.issues = issues
+#         self.business_help = business_help
+#         self.business_help_other = business_help_other
+#         self.jobs = jobs
+#         self.problem = problem
+#         self.needs = needs
+#         self.social = social
+#         self.email = email
+#         self.phone = phone
+#         self.company_name = company_name
+#         self.story = story
+#         self.coords = coords
+
     # company = Company(**request.json)
     # print(company)
     # company = {
@@ -50,67 +129,6 @@ def create_company():
     #         'lng': float(request.json['coords']['lng'])
     #     }
     # }
-    app.mongo.companyDB.create(request.json)
-    return JSONEncoder().encode(request.json), 201
-
-
-@app.flask.route("/api/v1/company/<company_id>", methods=["GET"])
-def get_company_by_id(company_id):
-    company = app.mongo.companyDB.get_company_by_id(company_id)
-    app.mongo.companyDB.get_all_markers()
-    return JSONEncoder().encode(company), 200
-
-
-@app.flask.route("/api/v1/getAllMarkers")
-def get_all_markers():
-    markers = app.mongo.companyDB.get_all_markers()
-    return JSONEncoder().encode(markers), 200
-
-
-class Company:
-    def __init__(self,
-                 type=None,
-                 name=None,
-                 address=None,
-                 business=None,
-                 business_type=None,
-                 issues=None,
-                 issues_other=None,
-                 help=None,
-                 help_other=None,
-                 business_help=None,
-                 business_help_other=None,
-                 jobs=None,
-                 problem=None,
-                 needs=None,
-                 social=None,
-                 email=None,
-                 phone=None,
-                 company_name=None,
-                 story=None,
-                 coords=None
-                    ):
-        self.type = type
-        self.name = name
-        self.address = address
-        self.business = business
-        self.issues_other = issues_other
-        self.help = help
-        self.help_other = help_other
-        self.business_type = business_type
-        self.issues = issues
-        self.business_help = business_help
-        self.business_help_other = business_help_other
-        self.jobs = jobs
-        self.problem = problem
-        self.needs = needs
-        self.social = social
-        self.email = email
-        self.phone = phone
-        self.company_name = company_name
-        self.story = story
-        self.coords = coords
-
 
 
 class JSONEncoder(json.JSONEncoder):
